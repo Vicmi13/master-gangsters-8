d3fc3625f377a9f81896d3aad1db21b0a9d167d4
Victor Torres <victor.torres@yalochat.com>
2021-08-16 23:34:35 -0500

    Added 2 part of webpack.configuration.js

--- KATA-REACT/webpack_exercise/dist/app.js
+++ KATA-REACT/webpack_exercise/dist/app.js
@@ -1,1 +1,96 @@
-(()=>{var e={720:(e,o,r)=>{"use strict";r.r(o),r.d(o,{default:()=>t});const t={greetingsArray:["Christian","Isaac","Carlos","Julio","Rodrigo"].map((e=>`Hola ${e}`)),objetoMaterias:{name1:"POO",name2:"DB",name3:"Infraestructura",name4:"IA"}}}},o={};function r(t){var n=o[t];if(void 0!==n)return n.exports;var a=o[t]={exports:{}};return e[t](a,a.exports,r),a.exports}r.d=(e,o)=>{for(var t in o)r.o(o,t)&&!r.o(e,t)&&Object.defineProperty(e,t,{enumerable:!0,get:o[t]})},r.o=(e,o)=>Object.prototype.hasOwnProperty.call(e,o),r.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},(()=>{const e=r(720);console.log("Direccion Cancun"),console.log("Estudiantes 1000"),console.log(e.greetingsArray)})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/info.js":
+/*!*********************!*\
+  !*** ./src/info.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst arrayName = [\"Christian\", \"Isaac\", \"Carlos\", \"Julio\", \"Rodrigo\"];\n\nconst objetoMaterias = {\n  name1: \"POO\",\n  name2: \"DB\",\n  name3: \"Infraestructura\",\n  name4: \"IA\",\n};\n\nconst greetings = arrayName.map((item) => `Hola ${item}`);\n\n// Forma en que exportamos con Node\n// module.exports = {\n//   greetings, // array\n//   materias: objetoMaterias\n// }\n\n// Forma babel, ECMA2015, JS viejito, common JS ¿?\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\n  greetingsArray: greetings,\n  objetoMaterias,\n});\n\n\n//# sourceURL=webpack://webpack_exercise/./src/info.js?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const info = __webpack_require__(/*! ./info */ \"./src/info.js\");\n\nconst objetoEscuela = {\n  name: \"de la vida\",\n  address: \"Cancun\",\n  students: 1000,\n  open: false,\n  rooms: 10,\n};\n\nconsole.log(`Direccion ${objetoEscuela.address}`);\nconsole.log(`Estudiantes ${objetoEscuela.students}`);\n\nconsole.log(info.greetingsArray);\n\n\n//# sourceURL=webpack://webpack_exercise/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	
+/******/ })()
+;
--- KATA-REACT/webpack_exercise/webpack.config.js
+++ KATA-REACT/webpack_exercise/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 /**
  * WEBPACK
  * entry
@@ -13,4 +14,27 @@ module.exports = {
     filename: "app.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      // las reglas que se cargan por medio de loaders
+      {
+        test: /\.html/, // lo que se tiene que buscar
+        use: [
+          // como lo va a aplicar
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      // filename: "./principal.html"
+    }),
+  ],
 };
