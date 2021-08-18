9b36840f678cccc892a5c5e8d7f1f3d4438b3c0c
Victor Torres <victor.torres@yalochat.com>
2021-08-17 20:40:20 -0500

    Proyecto linter

---
+++ KATA-REACT/linter_exercise/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+const name2 = "hola"
+
+const greeting = (param) => {
+	return `Martes ${param}`
+}
+
+console.log(greeting(name2))
+
+const nameArray = ["hola"]
+
+
+nameArray.map((item,i) =>  console.log(item))
+
+nameArray.map((item,i) =>  console.log(item))
+
