e2bc33a8a68f3f9fae9c0649ec9567d123090a11
Victor Torres <victor.torres@yalochat.com>
2021-08-25 18:45:40 -0500

    Se agregan props functions

--- KATA-REACT/react-class-project/src/App.jsx
+++ KATA-REACT/react-class-project/src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css';
-import Counter from './components/Counter';
-import ShowInfoCounter from './components/ShowInfoCounter';
-import React, { useState } from 'react'
+import "./App.css";
+import Counter from "./components/Counter";
+import ShowInfoCounter from "./components/ShowInfoCounter";
+import React, { useState } from "react";
 
 function App() {
-  const [nameValue, setNameValue] = useState('')
+  const [nameValue, setNameValue] = useState("");
 
-
-  const handleCounter = (name, age) => {
+  const handleCounter = (name) => {
     // this.setState({nameValue: name})
-    setNameValue(name)
+    setNameValue(name);
     // console.log('name PROPERTY', nameValue)
     // console.log('age PROPERTY', age)
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h3>Proyecto react con class components</h3>
-        <Counter counter={handleCounter}/>
+        <img src="" alt="" />
+        <Counter counter={handleCounter} />
         <ShowInfoCounter counter={nameValue} />
       </header>
     </div>
--- KATA-REACT/react-class-project/src/components/Counter.jsx
+++ KATA-REACT/react-class-project/src/components/Counter.jsx
@@ -2,38 +2,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
   state = {
     // llave: valor,
-    count: 30000
-  }
+    count: 30000,
+  };
 
-  // SE EJECUTA UNA SOLA VEZ 
+  // SE EJECUTA UNA SOLA VEZ
   componentDidMount() {
-    console.log('Componente montado y se puede manadar llamar api endpoint')
-    this.setState({ count : 0})
-  } 
+    console.log("Componente montado y se puede manadar llamar api endpoint");
+    this.setState({ count: 0 });
+  }
 
   // SE EJECUTA CADA VEZ QUE DETECTE UN CAMBIO EN PROPS O STATE
   componentDidUpdate() {
-    console.log('count en UPDATE', this.state.count)
+    console.log("count en UPDATE", this.state.count);
   }
 
   getCounterInfo() {
-    // primer refactor 
+    // primer refactor
     /*
     if (this.state.count === 0) {
       return 'tiene un  valor 0'
     } else return this.state.count
     */
-    return !this.state.count ? 'tiene un  valor 0' : this.state.count
+    return !this.state.count ? "tiene un  valor 0" : this.state.count;
   }
 
   incrementCounter() {
     // SE CAMBIA EL ESTADO
-    this.setState({ count: this.state.count +1 })
+    this.setState({ count: this.state.count + 1 });
     // PROPS name cualquier nombre
-    this.props.counter(this.state.count, 28)
+    this.props.counter(this.state.count);
   }
 
   render() {
@@ -46,7 +45,12 @@ class Counter extends Component {
         <section>
           <span>Counter {this.getCounterInfo()}</span>
         </section>
-        <button className="btn btn-warning" onClick={() => this.incrementCounter()}>Enviar</button>
+        <button
+          className="btn btn-warning"
+          onClick={() => this.incrementCounter()}
+        >
+          Enviar
+        </button>
       </div>
     );
   }
