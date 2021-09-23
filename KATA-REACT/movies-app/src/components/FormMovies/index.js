4a6e3edc4537866c4d808be78d1b3c4d844e9337
montoyaguzman7 <j.montoya@bbva.com>
2021-09-22 21:47:29 -0500

    FEAT : Eventos, funciones y estados genericos

--- KATA-REACT/movies-app/src/components/FormMovies/index.js
+++ KATA-REACT/movies-app/src/components/FormMovies/index.js
@@ -8,6 +8,9 @@ function FormMovies(props) {
     // 3 Mantener los estados que utiliza el formulario para actualizar valores
     const [movieName, setMovieName] = useState("");
     const [tickets, setTickets] = useState(0);
+    const [peliculas, setPeliculas] = useState({});
+
+    const [car, setCar] = useState("");
 
     const addMovie = () => {
         const moviesAux = { movieName, tickets };
@@ -35,13 +38,49 @@ function FormMovies(props) {
         setTickets(tickets);
     }
 
+    const handleSelect = (e) => {
+        const car = e.target.value;
+        console.log(`car ${car}`);
+        setCar(car);
+    }
+
+    const updateStatesForm = (e) => {
+        console.log('e: ', e.target.name);
+        const stateName = e.target.name;
+        const val = e.target.value;
+        console.log('val', val)
+
+        setPeliculas( { ...peliculas, [stateName] : val } );
+        console.log('cambio estado:', peliculas)
+    }
+
     return(
         <div>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Película</Form.Label>
-                <Form.Control type="text" placeholder="pelicula" onChange={(e) => movienameFn(e)}/>
+                <Form.Control type="text" name="moviename" placeholder="pelicula" onChange={(e) => updateStatesForm(e)}/>
+
+                {/*
+                    onChange={movienameFn} // ejecutarse cuando se ejecuta el evento, pero no envia parametros
+                    onChange={movienameFn()} // se autoejecuta en cuanto la carga del HTML llega a esa linea
+                    onChange={(e) => movienameFn(e)} // enviar parametro previniendo la autoejecucion, solo se ejecuta si el evento sucede
+                */}
+
                 <Form.Label>Boletos</Form.Label>
-                <Form.Control type="text" placeholder="boletos" onChange={(e) => ticketsFn(e)}/>
+                <Form.Control type="text" name="tickets" placeholder="boletos" onChange={(e) => updateStatesForm(e)}/>
+
+                <label for="cars">Choose a car:</label>
+                <textarea>vdsdvsdvdsvd</textarea>
+                
+                <br/>
+                <label for="cars">Choose a car:</label>
+                <select name="cars" id="cars" value={car} onChange={(e) => handleSelect(e)}>
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+
             </Form.Group>
             <Button variant="primary" onClick={() => addMovie()}>+</Button>
             <Button variant="primary" onClick={() => updateMovie()}>updateChanges</Button>
