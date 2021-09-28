aa870ac0ae385e649b89b9a009e23944f2cf3d42
Victor Torres Valencia <victor.tv@VictorTVs-MacBook-Pro.local>
2021-09-27 20:47:29 -0500

    Se agregan inputs y boton en el card de Login

--- KATA-REACT/login-authentication-app/src/components/Login.jsx
+++ KATA-REACT/login-authentication-app/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 // import Button from '@mui/material/Button';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock';
+import EmailIcon from '@mui/icons-material/Email';
 import './Login.css'
 
 function Login() {
@@ -15,6 +17,32 @@ function Login() {
                     <Typography variant="subtitle2" style={{ textAlign: "center", color: "#4f5052" }}>
                         Ingresar los datos requeridos
                     </Typography>
+
+                    <TextField
+                        className="input"
+                        required
+                        id="outlined-required"
+                        label="Email"
+                        InputProps={{
+                            endAdornment: (
+                                <EmailIcon />
+                            )
+                        }}
+                    />
+                    <TextField
+                        className="input"
+                        required
+                        id="outlined-required"
+                        label="Password"
+                        type="password"
+                        InputProps={{
+                            endAdornment: (
+                                <LockIcon />
+                            )
+                        }}
+                    />
+
+                    <Button variant="contained" style={{ marginTop: 80 }}>Enviar</Button>
                 </CardContent>
             </Card>
         </div>
