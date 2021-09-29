3f40020c52c689370d31c5b98675963e41d19262
Victor Torres Valencia <victor.tv@VictorTVs-MacBook-Pro.local>
2021-09-29 16:09:22 -0500

    Change BASE_URL

--- KATA-REACT/login-authentication-app/src/services/Auth.services.js
+++ KATA-REACT/login-authentication-app/src/services/Auth.services.js
@@ -1,23 +1,23 @@
-import axios from 'axios'
-import { URL_BACKEND } from '../utils/url_base'
+import axios from "axios";
+import { URL_BACKEND } from "../utils/url_base";
 
 const loginService = (formBody) => {
-    return new Promise( (resolve, reject) => {
-        axios.post(`${URL_BACKEND}/auth`, formBody)
-        .then(response => {
-            const { data } = response
-            // console.log('data', data)
-            // console.log('status', status)
-            resolve(data)
-        })
-        .catch( ({ response}) => {
-            /* Aplicamos destructuring */
-            // console.log('error', error.response)
-            // const { response} = error
-            reject(response)
-        })
-     })
-       
-}
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${URL_BACKEND}/login`, formBody)
+      .then((response) => {
+        const { data } = response;
+        // console.log('data', data)
+        // console.log('status', status)
+        resolve(data);
+      })
+      .catch(({ response }) => {
+        /* Aplicamos destructuring */
+        // console.log('error', error.response)
+        // const { response} = error
+        reject(response);
+      });
+  });
+};
 
-export default loginService
+export default loginService;
