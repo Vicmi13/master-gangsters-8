c043cf9abdfd61cf049b652d8f51756ee50cf573
maui <maui@devf.mx>
2021-10-25 21:13:42 -0500

    Agregar resumen de Test Driven Development

--- KATA-PATTERNS/testing/sum.test.js
+++ KATA-PATTERNS/testing/sum.test.js
@@ -14,3 +14,22 @@ test('[] + NaN to be equal to NaN', () => {
   const result = sum([], NaN);
   expect(result).toBe("NaN");
 })
+
+
+/**
+ * Resumen de TDD:
+ * 
+ * NO ES HACER PRIMERO TESTS,
+ * es hacer primero un proceso de análisis, y creación
+ * de un documento de diseño que te permite
+ * identificar QUÉ TESTS DEBES ESCRIBIR o mejor dicho
+ * QUÉ ESCENARIOS DEBES TOMAR EN CUENTA ANTES DE ESCRIBIR TU CÓDIGO
+ */
+
+test('se agrega una persona al tablero');
+
+test('se borra una persona del tablero');
+
+test('se manda una invitacion al tablero');
+
+test('se acepta una invitacion que ya expiro para unirse');
