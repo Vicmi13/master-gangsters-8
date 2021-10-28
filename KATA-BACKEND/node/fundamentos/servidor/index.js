12388235c815a7b59ea02fd136d0659567644283
montoyaguzman <montoyaguzman7@gmail.com>
2021-10-27 21:30:00 -0500

    FEAT : Servidor de paginas con node

--- KATA-BACKEND/node/fundamentos/servidor/index.js
+++ KATA-BACKEND/node/fundamentos/servidor/index.js
@@ -1,1 +1,43 @@
-// http y url
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    
+    const objectPath = url.parse(request.url);
+    const path = objectPath.pathname;
+    let completePath = '';
+
+    console.log('path', path);
+    
+    if(path == '/') {
+        completePath = 'static/index.html';
+    } else {
+        completePath = `static/${path}`;
+    }
+
+    fs.stat(completePath, error => {
+        if(!error) {
+            fs.readFile(completePath, (error, pagina) => {
+                if(!error) {
+                    response.writeHead(200, { 'Content-Type': 'text/html' } );
+                    response.write(pagina);
+                    response.end();
+                } else {
+                    response.writeHead(500, { 'Content-Type': 'text/plain' } );
+                    response.write('error en mi servidor');
+                    response.end();
+                }
+            })
+        } else {
+            // RETO
+            response.writeHead(404, { 'Content-Type': 'text/plain' } );
+            response.write('Not Found!');
+            response.end();
+        }
+    })
+
+});
+
+server.listen(8080);
+console.log('servidor iniciado...');
