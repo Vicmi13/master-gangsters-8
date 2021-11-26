3ca326167bafe1e9fce02376585278591a5447d8
Victor Torres Valencia <victor.tv@VictorTVs-MacBook-Pro.local>
2021-11-25 20:43:04 -0600

    Se hace refactor de modelo Shoe

--- KATA-BACKEND/knex-proyect/models/Shoes.js
+++ KATA-BACKEND/knex-proyect/models/Shoes.js
@@ -1,46 +1,19 @@
 // Se pasa la config || instancia de Knex
 const knexInstance = require("../config");
 
-const create = (bodyGenre) => {
-  // knex regresa una promesa
-  return knexInstance("shoes").insert(bodyGenre); // { description: 'male' }
-  // OUTPUT insert into `genres` (`description`) values ('male')
-};
+const genericModelKnex = require("../utils/GenericKnex");
 
-const getAll = () => {
-  return knexInstance("shoes").select(
-    "shoe_id",
-    "description",
-    "sold",
-    "quantity",
-    "created_at",
-    "updated_at"
-  );
-};
+const table = "shoes";
+const columns = [
+  "shoe_id",
+  "description",
+  "sold",
+  "quantity",
+  "created_at",
+  "updated_at",
+];
+const tableId = "shoe_id";
 
-const getById = (id) => {
-  // return knexInstance("genres").select("id", "description").where({ id });
+const Shoe = genericModelKnex(knexInstance, table, columns, tableId);
 
-  return knexInstance("shoes")
-    .where({ shoe_id: id })
-    .select("shoe_id", "description", "sold", "quantity", "created_at");
-};
-
-const updateById = (id, body) => {
-  // console.log("body in model", body);
-  // console.log("id", id);
-  return knexInstance("shoes").where({ shoe_id: id }).update(body);
-};
-
-// Delete FISICO
-const deleteById = (id) => {
-  return knexInstance("shoes").del().where({ shoe_id: id });
-};
-
-module.exports = {
-  create,
-  getAll,
-  getById,
-  updateById,
-  deleteById,
-};
+module.exports = Shoe;
