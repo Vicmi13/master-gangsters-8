81d52fd43af6f58c5b833485bbd0095410cf6182
Victor Torres Valencia <victor.tv@wizeline.com>
2022-04-18 21:31:31 -0500

    Se dockeriza proyecto de api-mongo-express

---
+++ KATA-BACKEND/api-mongo-express/docker.commands.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * NOTA 18/04/22
+ * Docker 7.0 tiene un bug en Windows
+ * downgrade a la versión 4.4.4
+ *
+ * 
+ * Comando global de docker
+ * $ docker
+ * 
+ * 
+ * Descar una imagen de docker hub
+ * $ docker pull image_name
+ * 
+ * 
+ * Corre una imagen de docker
+ * $ docker run image_name
+ * 
+ *  $ docker run image_name -p 0000:000 
+ * 
+ * 
+ * Muestra las imagenes de docker instaladas
+ * $ docker images
+ * 
+ * 
+ * Muestra los contenedores corriendo
+ * $ docker ps
+ * 
+ * 
+ * Ejecutar una imagen de docker
+ * $ docker run image_name
+ * 
+ * 
+ * Detener un contenedor de docker
+ * $ docker stop image_container_id
+ *  
+ */
