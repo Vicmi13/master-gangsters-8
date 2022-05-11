ec5a0aabbc58544624a2cad5db4920f87b2b1ee7
Victor Torres Valencia <victor.tv@wizeline.com>
2022-05-11 18:49:53 -0500

    Complete MSW testing

--- testing/services/currency.js
+++ testing/services/currency.js
@@ -3,8 +3,9 @@ const { EXCHANGE_BASE_URL } = require("../utils/const");
 
 const convertion = async (base, destination) => {
   const result = await axios.get(`${EXCHANGE_BASE_URL}/latest?base=${base}`);
-  console.log("result", result);
-  return result.data[destination];
+  console.log("result", result.data);
+  // result.data.rates
+  return result.data.rates[destination];
 };
 
 module.exports = convertion;
--- testing/services/currency.spec.js
+++ testing/services/currency.spec.js
@@ -7,17 +7,35 @@ const { EXCHANGE_BASE_URL } = require("../utils/const");
 const convertion = require("./currency");
 
 describe("Currency service", () => {
+  // initialize server
+
+  beforeAll(() => {
+    console.log(server);
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers);
+
   const server = setupServer(
-    rest.get(`${EXCHANGE_BASE_URL}/latest`, (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json({ rates: { MXN: 42.5 } }));
+    rest.get(`${EXCHANGE_BASE_URL}/latest`, (_, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ rates: { CAD: 27.4 } }));
     })
   );
 
-  // initialize server
-  beforeAll(() => server.listen());
-
   it("makes convertion properly", async () => {
     const result = await convertion("USD", "CAD");
-    expect(result).toEqual(42.7);
+    expect(result).toEqual(27.4);
+  });
+
+  it("handles  error properly", async () => {
+    server.use(
+      rest.get(`${EXCHANGE_BASE_URL}/latest`, (req, res, ctx) => {
+        return res(ctx.status(404));
+      })
+    );
+
+    await expect(convertion("USD", "CAD")).rejects.toThrow(
+      "Request failed with status code 404"
+    );
   });
 });
