a47a281e3302ac0a34b1fd176c424ef95f991105
Victor Torres Valencia <victor.tv@wizeline.com>
2022-05-10 19:11:39 -0500

    Added ack option for receiver file

--- KATA-BACKEND/first-queue-project/publisher.js
+++ KATA-BACKEND/first-queue-project/publisher.js
@@ -1,40 +1,42 @@
-
 /**
  * 1- Create connection
  * 2- Create channel
  * 3- Assser queue
  * 4- Send message to queue
- * 
- * 
+ *
+ *
  * Publisher also known as producer
  *
  */
 
-const amqp = require("amqplib/callback_api")
+const amqp = require("amqplib/callback_api");
 
 // step 1 create connection
 amqp.connect("amqp://localhost", (errorConnection, connection) => {
-    if (errorConnection) {
-         throw errorConnection
-        // const obj = { name: 'falla'}
-        // const objError = { mmsg: 'Algo anda mal'}    
-        // if (obj.name === 'falla') throw objError
-    }
+  if (errorConnection) {
+    throw errorConnection;
+    // const obj = { name: 'falla'}
+    // const objError = { mmsg: 'Algo anda mal'}
+    // if (obj.name === 'falla') throw objError
+  }
 
-    //step 2 create channel
-    connection.createChannel( (errorChannel, channel) => {
-        if (errorChannel) throw errorChannel
+  //step 2 create channel
+  connection.createChannel((errorChannel, channel) => {
+    if (errorChannel) throw errorChannel;
 
-        //step 3 assert queue
-        const QUEUE_NAME = 'generacion8'
-        channel.assertQueue(QUEUE_NAME)
+    //step 3 assert queue
+    const QUEUE_NAME = "generacion8";
+    channel.assertQueue(QUEUE_NAME, { durable: true });
 
-        //step 4 Send message to queue
+    //step 4 Send message to queue
 
-        channel.sendToQueue(QUEUE_NAME, Buffer.from('hola mundo desde mi primer cola') )
+    channel.sendToQueue(
+      QUEUE_NAME,
+      Buffer.from("hola mundo desde mi primer cola")
+    );
 
-        console.log(`message send to queue ${QUEUE_NAME} 📝`)
+    console.log(`message send to queue ${QUEUE_NAME} 📝`);
+  });
 
-
-    })
-})
+  setTimeout(() => connection.close(), 2000);
+});
--- KATA-BACKEND/first-queue-project/receiver.js
+++ KATA-BACKEND/first-queue-project/receiver.js
@@ -1,37 +1,47 @@
-
 /**
  * 1- Create connection
  * 2- Create channel
  * 3- Assser queue
  * 4- Receive message
- * 
- * 
+ *
+ *
  * Receiver also known as consumer
  *
  */
 
- const amqp = require("amqplib/callback_api")
+const amqp = require("amqplib/callback_api");
 
- // step 1 create connection
+// step 1 create connection
 
- amqp.connect("amqp://localhost", (errorConnection, connection) => {
-     if (errorConnection) {
-          throw errorConnection
-     }
- 
-     //step 2 create channel
-     connection.createChannel( (errorChannel, channel) => {
-         if (errorChannel) throw errorChannel
- 
-         //step 3 assert queue
-         const QUEUE_NAME = 'generacion8'
-         channel.assertQueue(QUEUE_NAME)
- 
-        //step 4 Receive messages
-        channel.consume(QUEUE_NAME, (msg) => {
-            console.log('message received', msg.content.toString())
-        })
- 
- 
-     })
- })
+amqp.connect("amqp://localhost", (errorConnection, connection) => {
+  if (errorConnection) {
+    throw errorConnection;
+  }
+
+  //step 2 create channel
+  connection.createChannel((errorChannel, channel) => {
+    if (errorChannel) throw errorChannel;
+
+    //step 3 assert queue
+    const QUEUE_NAME = "generacion8";
+    channel.assertQueue(QUEUE_NAME);
+
+    //step 4 Receive messages
+    channel.consume(
+      QUEUE_NAME,
+      (msg) => {
+        console.log("message received", msg.content.toString());
+
+        // acknowledgment explicito
+        channel.ack(msg);
+      }
+
+      // acknowledgment implicito
+      //   {
+      //     noAck: true,
+      //   }
+    );
+  });
+
+  // setTimeout(() => connection.close(), 2000);
+});
